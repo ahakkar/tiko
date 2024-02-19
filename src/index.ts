0b80c3ee3d341d06ac5ff90e438a16e5edc1a71a
Antti Pham <antti.pham@tuni.fi>
2024-02-19 17:05:03 +0200

    Initial server files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-console.log('testii!');
-console.log('testii2!');
-console.log('testii3!');
+import dotenv from 'dotenv';
+import express, {Express} from 'express';
+import morgan from 'morgan';
+import t1 from './controller/t1';
+
+dotenv.config();
+const app: Express = express();
+const port = process.env['PORT'];
+
+// Middlewaret
+app.use(morgan('tiny')); // Logger
+
+// Routerit
+app.use('/t1', t1);
+
+// Staattiset tiedostot
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
