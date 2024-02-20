29121b869d93b33bc9e0abb64c125504839bf2c1
Antti Hakkarainen <a.hakkarainen@iki.fi>
2024-02-20 11:29:46 +0200

    poista käyttämättömät importit worksitestä

--- src/controller/worksites.ts
+++ src/controller/worksites.ts
@@ -1,4 +1,3 @@
-import {WorkSiteType} from '../constants/workSiteTypes';
 import {Router} from 'express';
 
 const router = Router();
@@ -10,15 +9,5 @@ router.get('/', (_req, res) => {
 router.post('/', (_req, res) => {
   res.send('<div>Työkohde on lisätty asiakkaalle.</div>');
 });
-
-function generateWorkSites(): string {
-  // populate the dropdown with work sites from WorkSiteTypes
-  let dd = '<select name="worksite_type" id="workSiteDropdown">';
-  Object.values(WorkSiteType).forEach(workSite => {
-    dd += `<option value="${workSite}">${workSite}</option>`;
-  });
-  dd += '</select>';
-  return dd;
-}
 
 export default router;
--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,9 @@ app.use(express.urlencoded({extended: true})); // Parse POST data
 
 // Routerit
 app.use('/customers', customers);
-app.use('/products', customers);
-app.use('/projects', customers);
-app.use('/worksites', customers);
+app.use('/products', products);
+app.use('/projects', projects);
+app.use('/worksites', worksites);
 
 // Staattiset tiedostot
 app.use(express.static('public'));
