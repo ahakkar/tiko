49420fac02f42b3d82f16e08670cdf09951a27af
Kilian <kilian.kugge@tuni.fi>
2024-02-27 13:52:27 +0200

    Luo yhteyspistemoduuli pg tietokantatapahtumille

---
+++ src/models/db/index.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import { Pool, PoolClient, QueryResult } from "pg";
+
+dotenv.config();
+
+const pool = new Pool({
+    database: process.env['POSTGRES_USER'],
+    host: process.env['POSTGRES_HOST'],
+    user: process.env['POSTGRES_USER'],
+    password: process.env['POSTGRES_PASSWORD']
+});
+
+/**
+ * Funktio yksittäisen kyselyn tekemistä varten.
+ * @param text Kysely
+ * @returns Palauttaa Promisen, joka sisältää kyselyn tuloksen
+ */
+export function query(text: string): Promise<QueryResult<any>> {
+    return pool.query(text);
+}
+
+/**
+ * Palauttaa yhteyden tietokantaan monivaiheisia transaktioita varten
+ * @returns Promise, joka sisältää yhteyden tietokantaan
+ */
+export function getClient(): Promise<PoolClient> {
+    return pool.connect();
+}
