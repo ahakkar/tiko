4f895eed115b831c0b690a413e45326d675c0220
Antti Pham <antti.pham@tuni.fi>
2024-03-20 23:37:41 +0200

    Vaihda työntekijän salasanatiiviste

--- dbinit/3. users.sql
+++ dbinit/3. users.sql
@@ -6,5 +6,7 @@ CREATE TABLE kayttajat (
 
 INSERT INTO kayttajat (nimi, salasanatiiviste, rooli)
 VALUES
+-- Vaikka salasanat ovat molemmilla "salasana", salasanatiivisteet näyttävät
+-- erilaisilta, mikä on tietoturvan kannalta erinomaista.
 ('admin', '$2b$10$6AxDO9gw5L.j2RCuC01ODega3at3S0Z7ksHyA7eiHOk0uQBI/Hy2q', 'write'),
-('työntekijä', '$2b$10$6AxDO9gw5L.j2RCuC01ODega3at3S0Z7ksHyA7eiHOk0uQBI/Hy2q', 'read');
+('työntekijä', '$2b$10$5evxfBmq3gmDZg.r/TrHmOd6Aza9C4ewMdaWbuhEu/CnO7mEk3Xta', 'read');
