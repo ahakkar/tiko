83845b88721aa185e7ee3fad78b91a7ab0b1b171
Kilian <kilian.kugge@tuni.fi>
2024-03-21 10:17:09 +0200

    Yhtenäistä laskun luomisen reititys

--- src/controllers/laskutController.ts
+++ src/controllers/laskutController.ts
@@ -17,42 +17,30 @@ router.post('/', async (req, res) => {
     res.sendStatus(StatusCode.Unauthorized);
     return;
   }
-  // TODO: Tee muistutuslasku/karhulasku
-  // Jos bodysta puuttuu summa ja sisältää edellinen_lasku-kentän:
-  // {era_pvm: '2024-03-30', edellinen_lasku: '1'},
-  // tee muistutuslasku
-  const l: Lasku = {
-    tyosuoritus_id: Number(req.body.tyosuoritus_id),
-    summa: Number(req.body.summa),
-    pvm: new Date(),
-    era_pvm: new Date(req.body.era_pvm),
-  };
-  console.log('tallennetaan lasku', req.body);
 
-  if (!validoiLasku(l)) {
-    res.sendStatus(StatusCode.BadRequest);
-    return;
+  // Jos laskulla on edellinen lasku, se on muistutuslasku.
+  if (req.body.edellinen_lasku) {
+    await addMuistutusLasku(req.body.era_pvm, req.body.edellinen_lasku);
+  } else {
+    const l: Lasku = {
+      tyosuoritus_id: Number(req.body.tyosuoritus_id),
+      summa: Number(req.body.summa),
+      pvm: new Date(),
+      era_pvm: new Date(req.body.era_pvm),
+    };
+    console.log('tallennetaan lasku', req.body);
+
+    if (!validoiLasku(l)) {
+      res.sendStatus(StatusCode.BadRequest);
+      return;
+    }
+
+    if (!(await lisaaLasku(l))) {
+      res.sendStatus(StatusCode.InternalServerError);
+      return;
+    }
   }
 
-  if (!(await lisaaLasku(l))) {
-    res.sendStatus(StatusCode.InternalServerError);
-    return;
-  }
-
-  res.set('hx-refresh', 'true');
-  res.sendStatus(StatusCode.OK);
-});
-
-// TODO: Ei ole REST:n mukainen endpoint. Mieluiten /laskut alle
-// eikä /laskut/muistutus alle. Endpoint /laskut/muistutus
-// tarkoittaa, että jokaisen laskun alla on 1 kpl oma muistutuksensa.
-router.post('/muistutus', async (req, res) => {
-  if (!res.locals['writeAccess']) {
-    res.sendStatus(StatusCode.Unauthorized);
-    return;
-  }
-
-  await addMuistutusLasku(req.body.era_pvm, req.body.edellinen_lasku);
   res.set('hx-refresh', 'true');
   res.sendStatus(StatusCode.OK);
 });
