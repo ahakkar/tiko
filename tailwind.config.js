63aef550ff240be7301bb376480e4ca592b2d411
Antti Hakkarainen <a.hakkarainen@iki.fi>
2024-04-03 17:23:40 +0300

    korjaa hajottamani linkit napeissa concatilla

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    colors: {
+      tarskyLt: {
+        light: '#e2e8f0',
+        DEFAULT: '#e2e8f0',
+        dark: '#334155',
+      },
+      tarskyMed: {
+        light: '#cbd5e1',
+        DEFAULT: '#cbd5e1',
+        dark: '#0f172a',
+      },
+      tarskyBlueLt: {
+        light: '#bfdbfe',
+        DEFAULT: '#bfdbfe',
+        dark: '#2563eb',
+      },
+      tarskyBlueMed: {
+        light: '#3b82f6',
+        DEFAULT: '#3b82f6',
+        dark: '#1e3a8a',
+      },
+      white: {
+        light: '#ffffff',
+        DEFAULT: '#ffffff',
+        dark: '#d6d6d6',
+      },
+    },
   },
-  plugins: [],
 };
