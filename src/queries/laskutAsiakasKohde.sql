209b5468c5068ba9659713a05d271160ab129044
Antti Hakkarainen <a.hakkarainen@iki.fi>
2024-04-08 09:50:31 +0300

    käytä colspania useamman <td> sijasta

--- src/queries/laskutAsiakasKohde.sql
+++ src/queries/laskutAsiakasKohde.sql
@@ -1,15 +1,16 @@
 SELECT 
     l.id,
-    ts.id AS tyosuoritus_id,
+    l.tyosuoritus_id,
     a.nimi AS asiakas_nimi,
     tk.osoite AS tyokohde_osoite,
     tk.postitoimipaikka AS tyokohde_postitoimipaikka,
     l.pvm,
     l.era_pvm,
-    l.summa,
+    l.yhteissumma,
     CASE WHEN l.maksettu_pvm IS NULL THEN false ELSE true END AS is_maksettu 
-FROM lasku l
+FROM koko_lasku l
 JOIN tyosuoritus ts ON ts.id = l.tyosuoritus_id
 JOIN asiakas a ON a.id = ts.asiakas_id
-JOIN tyokohde tk ON tk.id = ts.tyokohde_id;
+JOIN tyokohde tk ON tk.id = ts.tyokohde_id
+ORDER BY l.id;
 
